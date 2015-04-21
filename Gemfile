4487373dad4aa8000d8d6aab081b2488d2fddef5
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-04-21 22:32:10 +0200

    Chat app to test websockets on heroku

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+#ruby "2.1.5"
+
+gem "faye-websocket"
+gem "sinatra"
+gem "puma"
+gem "redis"
---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require 'tilt/erb'
+
+module ChatDemo
+  class App < Sinatra::Base
+    get "/" do
+      erb :"index.html"
+    end
+
+    get "/assets/js/application.js" do
+      content_type :js
+      @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
+      erb :"application.js"
+    end
+  end
+end
