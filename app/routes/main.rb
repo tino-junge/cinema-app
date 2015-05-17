74afb29833eb12933ae28ba5062ae9afcd59fc3d
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-05-17 22:47:15 +0200

    Split movie and voting in separate js files

--- app/routes/main.rb
+++ app/routes/main.rb
@@ -4,22 +4,29 @@ module ActiveCinema
       slim :index
     end
 
-    get "/js/application.js" do
-      content_type :js
-      @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
-      # Only Testing
-      @video_link_a = settings.video.sequels[0].stream
-      @video_link_b = settings.video.sequels[1].stream
-      erb :"application.js"
+    get "/movie" do
+      @video = ActiveCinema.the_video
+      slim :movie
     end
 
     get "/voting" do
       slim :voting
     end
 
-    get "/movie" do
-      @video = settings.video
-      slim :movie
+    get "/js/application.js" do
+      content_type :js
+      @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
+      erb :"application.js"
+    end
+
+    get "/js/movie.js" do
+      content_type :js
+      erb :"movie.js"
+    end
+
+    get "/js/voting.js" do
+      content_type :js
+      erb :"voting.js"
     end
   end
 end
